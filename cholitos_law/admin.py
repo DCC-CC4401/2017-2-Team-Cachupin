270ac3a5eed4e12d243c7ffba333ef157e40fa95
Kaminari <camrojas@ug.uchile.cl>
2017-09-27 00:42:23 -0300

    Edit animals, minicipalities and complaints models with various parameters, and make migrations

--- cholitos_law/admin.py
+++ cholitos_law/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 from .models import Animal
+from .models import Municipality
+from .models import Complaint
 
 admin.site.register(Animal)
+admin.site.register(Municipality)
+admin.site.register(Complaint)
--- cholitos_law/models.py
+++ cholitos_law/models.py
@@ -6,6 +6,12 @@ from django.utils import timezone
 class Animal(models.Model):
     name = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    image = models.CharField(max_length=200, default='')
+    birth =  models.DateTimeField()
+    description = models.CharField(max_length=500, default='')
+    gender = models.CharField(max_length=10, default='hembra')
+
+    animals_options = ('Perro', 'Gato', 'Otro')
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
@@ -17,6 +23,9 @@ class Municipality(models.Model):
     name = models.CharField(max_length=200)
     commune = models.CharField(max_length=100)
     region = models.CharField(max_length=200)
+    image = models.CharField(max_length=200)
+    latitud = models.FloatField()
+    longitud = models.FloatField()
 
     def __str__(self):
         return self.name
@@ -26,3 +35,16 @@ class Complaint(models.Model):
     animal_type = models.CharField(max_length=100)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    gender = models.CharField(max_length=10)
+    hurt = models.BooleanField()
+    comment = models.CharField(max_length=500)
+    image = models.CharField(max_length=200)
+    rescue = models.BooleanField()
+
+    types_options = ('Abandono de Calle',
+                     'Exposición a temperaturas extremas',
+                     'Falta de Agua',
+                     'Falta de Comida',
+                     'Violencia',
+                     'Venta ambulante')
+    animals_options = ('Perro', 'Gato', 'Otro')
